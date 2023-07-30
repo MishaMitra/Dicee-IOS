491f6afbd52623187cd831c8e8dd37cd5afc1dec
Misha Mitra <babbilonterror@gmail.com>
2023-07-30 21:10:11 +0400

    Initial commit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
-
+       /*IBOutlets is allow me to reference UI elements from Interface Builder and change it from  code to design file*/
 class ViewController: UIViewController {
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+   /*IBAction is allow me to limk triggers from screen (presses buttons) and put this information into my code*/
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        // diceArray is array of assets (images of dice with numbers 1..6). We use let - cause we don't need to mutate our Array
+        let diceArray = [UIImage(imageLiteralResourceName: "DiceOne"),UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")]
+        
+        // here we randomly cahnge our image of dices (left and right) in two different ways of codding using our diceArray
+        diceImageViewOne.image = diceArray.randomElement()
+        diceImageViewTwo.image = diceArray [Int.random (in: 0...5)]
     }
-
-
 }
 
